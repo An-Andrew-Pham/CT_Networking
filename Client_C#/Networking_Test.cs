61c6dad4d02b9d6964506cb7cbfed40558a3433f
unknown <phaman@usc.edu>
2018-11-14 16:47:48 -0800

    Array Byte Method for Java Server and C# Client

--- Client_C#/Networking_Test.cs
+++ Client_C#/Networking_Test.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 using UnityEngine.Networking; //Method 1: Use Unity's API? 
 using System;
 using System.IO;
-using System.Web.Script.Serialization;
+//using System.Web;
+//using System.Web.Script.Serialization;
+using System.Net;
 using System.Net.Sockets; //Method 2: Use C# standard networking library?  
 
 public class Networking_Test : MonoBehaviour {
     public bool isAtStartup = true;
    // NetworkClient myClient;
-    TCPClient myClient;
+    ClientSocket myClient;
     // Use this for initialization
     void Start () {
-        myClient = new TCPClient("127.0.0.1", 6789); //6789 is set, but IP address is different 
+        myClient = new ClientSocket("127.0.0.1", 6789); //6789 is set, but IP address is different 
         
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
+        myClient.Send();   
     }
 
     // Create a client and connect to the server port
@@ -38,18 +40,55 @@ public class Networking_Test : MonoBehaviour {
     }
 }
 
-class TCPClient
+class ClientSocket
 {
-private TcpClient Client;
-private NetworkStream Stream;
+    //private TcpClient Client;
+    //private NetworkStream Stream;
+    Socket Client;
+    private Byte[] Data;
 
-private Byte[] Data;
+    public ClientSocket(string address, int port)
+    {
+        //Client = new TcpClient();
+        //Client.Connect(address, port);
 
-public TCPClient(string address, int port)
-{
-    Client = new TcpClient();
-    Client.Connect(address, port);
+        IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6789);
 
-}
+        Socket Client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
+        try
+        {
+            Client.Connect(ip);
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("Unable to connect to server.");
+            return;
+        }
+
+    }
+
+    public void Send()
+    {
+        string toSend = "Hello!";
+
+        // Sending
+        int toSendLen = System.Text.Encoding.ASCII.GetByteCount(toSend);
+        byte[] toSendBytes = System.Text.Encoding.ASCII.GetBytes(toSend);
+        byte[] toSendLenBytes = System.BitConverter.GetBytes(toSendLen);
+        //Client.Send(toSendLenBytes);
+        Client.Send(toSendBytes);
+
+        // Receiving
+       /* byte[] rcvLenBytes = new byte[4];
+        Client.Receive(rcvLenBytes);
+        int rcvLen = System.BitConverter.ToInt32(rcvLenBytes, 0);
+        byte[] rcvBytes = new byte[rcvLen];
+        Client.Receive(rcvBytes);
+        String rcv = System.Text.Encoding.ASCII.GetString(rcvBytes);
+
+        Console.WriteLine("Client received: " + rcv);
+        */
+        Client.Close();
+    }
 };
--- GameDataServer/src/Server/GameServer.java
+++ GameDataServer/src/Server/GameServer.java
@@ -3,8 +3,12 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Vector;
+import java.io.PrintWriter;
+import com.google.gson.Gson;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-public class GameServer {
+public class GameServer{
 	
 	private Vector<String> playerThreads;
 	
@@ -17,15 +21,41 @@ public class GameServer {
 			//no number error 
 			System.out.println("Bound to port " + port);
 			playerThreads = new Vector<String>();
-			while(true)
-			{
-				Socket s = ss.accept(); //blocking line code -- always wait for user to connect
-				System.out.println("Connection from " + s.getInetAddress());
-				//Chatthreat st = new Chatthread(s); //instantiated for every new client 
-				//To broadcoast message for all ur clients - u gotta iterate through all
-				//ur server threads
-				//serverThreads.add(st); 
-			}
+			
+			Socket s = ss.accept(); //blocking line code -- always wait for user to connect
+			System.out.println("Connection from " + s.getInetAddress());
+			
+			InputStream is = s.getInputStream();
+	        //OutputStream os = s.getOutputStream();
+	        
+	        // Receiving
+	       /* byte[] lenBytes = new byte[4];
+	        is.read(lenBytes, 0, 4);
+	        int len = (((lenBytes[3] & 0xff) << 24) | ((lenBytes[2] & 0xff) << 16) |
+	                  ((lenBytes[1] & 0xff) << 8) | (lenBytes[0] & 0xff));
+	        byte[] receivedBytes = new byte[len];*/
+			byte[] receivedBytes = new byte[4];
+	        is.read(receivedBytes);
+	        String received = new String(receivedBytes, 0, 32);
+
+	        System.out.println("Server received: " + received);
+	        
+	        // Sending
+	       /* String toSend = "Echo: " + received;
+	        byte[] toSendBytes = toSend.getBytes();
+	        int toSendLen = toSendBytes.length;
+	        byte[] toSendLenBytes = new byte[4];
+	        toSendLenBytes[0] = (byte)(toSendLen & 0xff);
+	        toSendLenBytes[1] = (byte)((toSendLen >> 8) & 0xff);
+	        toSendLenBytes[2] = (byte)((toSendLen >> 16) & 0xff);
+	        toSendLenBytes[3] = (byte)((toSendLen >> 24) & 0xff);
+	        os.write(toSendLenBytes);
+	        os.write(toSendBytes);*/
+
+	        s.close();
+	        ss.close();
+	 
+			
 		}catch(IOException ioe) {
 			System.out.println("ioe " + ioe.getMessage() );
 		}finally {
@@ -40,6 +70,29 @@ public class GameServer {
 			
 		}
 	}
+	
+	/*private BufferedReader recv;
+
+	public void run()
+	{
+	
+        try 
+        {
+            clientSocket = serverSocket.accept();
+            System.out.println("Client Connected from " + clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort());
+
+            recv = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("Data Recieved: " + recv.readLine());
+
+            clientSocket.close();
+        }
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+	    
+	}*/
 	
 	public static void main(String [] args)
 	{
