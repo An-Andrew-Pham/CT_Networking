e889c9b77525711ddd0d463cada8c6aec1d874ef
unknown <phaman@usc.edu>
2018-11-15 20:29:21 -0800

    Current working one (connect, transfer data)

---
+++ Client_C#/Client_Networking.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client_Networking : MonoBehaviour {
+
+    ClientSocket mySocket;
+
+	// Use this for initialization
+	void Start () {
+        mySocket = new ClientSocket();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
+
+class ClientSocket
+{
+    public ClientSocket()
+    {
+        string toSend = "Hello!";
+
+        IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("10.26.75.211"), 4343);
+
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        clientSocket.Connect(serverAddress);
+
+        // Sending
+        int toSendLen = System.Text.Encoding.ASCII.GetByteCount(toSend);
+        byte[] toSendBytes = System.Text.Encoding.ASCII.GetBytes(toSend);
+        byte[] toSendLenBytes = System.BitConverter.GetBytes(toSendLen);
+        clientSocket.Send(toSendLenBytes);
+        clientSocket.Send(toSendBytes);
+
+        // Receiving
+        byte[] rcvLenBytes = new byte[4];
+        clientSocket.Receive(rcvLenBytes);
+        int rcvLen = System.BitConverter.ToInt32(rcvLenBytes, 0);
+        byte[] rcvBytes = new byte[rcvLen];
+        clientSocket.Receive(rcvBytes);
+        String rcv = System.Text.Encoding.ASCII.GetString(rcvBytes);
+
+        Debug.Log("Client received: " + rcv);
+
+        clientSocket.Close();
+    }
+}
